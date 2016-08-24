63527dff3ccc6a2806b0a68666587083c3148c89
Ahmad Awais <mrahmadawais@gmail.com>
2016-08-24 15:05:58 +0500

    Function Check and PHP Coding Standards Compliant 💯

--- functions.php
+++ functions.php
@@ -1,24 +1,39 @@
 <?php
 /**
- * Child Theme Functions
+ * Child theme functions
+ *
+ * Functions file for child theme, enqueues parent and child stylesheets by default.
+ *
+ * @since 	1.0.0
+ * @package _child
  */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-/*
- * Enqueue the parent style.css
- *
- * twentyfifteen parent theme for twentfifteen-child
- *
- */
-add_action( 'wp_enqueue_scripts', 'aa_theme_enqueue_styles' );
-function aa_theme_enqueue_styles() {
+if ( ! function_exists( 'aa_enqueue_styles' ) ) {
+	// Add enqueue function to the desired action.
+	add_action( 'wp_enqueue_scripts', 'aa_enqueue_styles' );
 
-    // Parent style variable
-    $parent_style = 'parent-style';
-    // Enqueue Parent theme's style
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	/**
+	 * Enqueue Styles.
+	 *
+	 * Enqueue parent style and child styles where parent are the dependency
+	 * for child styles so that parent styles always get enqueued first.
+	 *
+	 * @since 1.0.0
+	 */
+	function aa_enqueue_styles() {
+		// Parent style variable.
+		$parent_style = 'parent-style';
 
-    // Enqueue Child theme's style and ensure it is
-    // Setting 'parent-style' as a dependency will ensure that the child theme stylesheet loads after it.
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+		// Enqueue Parent theme's stylesheet.
+		wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+		// Enqueue Child theme's stylesheet.
+		// Setting 'parent-style' as a dependency will ensure that the child theme stylesheet loads after it.
+		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+	}
 }
